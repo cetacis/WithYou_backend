986050fb1a9f9c74c3359be03dc7f8bbc848ee20
Unalian <Dengmengda@gmail.com>
2020-07-17 18:00:41 +0800

    usual push

--- main.go
+++ main.go
@@ -1,31 +1,75 @@
 package main
 
 import (
-	"./src"
-	"github.com/iris-contrib/middleware/cors"
+	"context"
 	"github.com/kataras/iris"
-	"github.com/kataras/iris/middleware/logger"
-	"github.com/kataras/iris/middleware/recover"
+	"go.mongodb.org/mongo-driver/bson"
+	"io"
+	"os"
 )
 
-func main() {
-	src.ConnectDB()
-	defer src.DisconnectDb()
-	app := iris.New()
-	app.Use(recover.New())
-	app.Use(logger.New())
+const FilePath = "./img"
 
-	crs := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowCredentials: true,
-	})
+func Register(ctx iris.Context) {
+	// get info
+	Name := ctx.FormValue("name")
+	Email := ctx.FormValue("email")
+	Pass := ctx.FormValue("pass")
+	// get img
+	file, info, err := ctx.FormFile("img")
+	if err != nil {
+		ctx.StatusCode(iris.StatusInternalServerError)
+		ctx.Application().Logger().Warnf("Error while uploading: %v", err.Error())
+		return
+	}
+	defer file.Close()
+	filename := info.Filename
+	out, err := os.OpenFile(FilePath + filename, os.O_WRONLY | os.O_CREATE, 0666)
+	if err != nil {
+		ctx.StatusCode(iris.StatusInternalServerError)
+		ctx.Application().Logger().Warnf("Error while preparing the new file: %v", err.Error())
+		return
+	}
+	defer out.Close()
+	_, _ = io.Copy(out, file)
+	ImgPath := FilePath + filename
 
-	api := app.Party("/api", crs).AllowMethods(iris.MethodOptions)
+	// create User info
+	UserData := User {
+		Username: Name,
+		Email: Email,
+		Password: Pass,
+		ImgPath: ImgPath,
+	}
+	var result User
+	collection := Client.Database("WithYou").Collection("UserInfo")
 
-	api.Post("/login", src.Login)
+	// find email
+	filter := bson.M{"email": Email}
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err == nil {
+		RtData := RtMsg {
+			Msg: "Email has existed",
+			Code: -1,
+		}
+		_, _ = ctx.JSON(RtData)
+		return
+	}
 
-	api.Post("/register", src.Register)
+	// insert register data
+	_, err = collection.InsertOne(context.TODO(), UserData)
+	if err != nil {
+		RtData := RtMsg {
+			Msg: "Register error",
+			Code: 1,
+		}
+		_, _ = ctx.JSON(RtData)
+		return
+	}
 
-	//_ = app.Run(iris.Addr(":8080"))
-
+	RtData := RtMsg {
+		Msg: "Register succeed",
+		Code: 0,
+	}
+	_, _ = ctx.JSON(RtData)
 }
