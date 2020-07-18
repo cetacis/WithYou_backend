c46ba382614bd653e1c25fe5595c6e2786b8ae88
kelolemon <cutesinapis@gmail.com>
2020-07-18 15:14:03 +0800

    fuck new learner

--- main.go
+++ main.go
@@ -1,61 +1,28 @@
 package main
 
 import (
-	"context"
+	"./src"
+	"github.com/iris-contrib/middleware/cors"
 	"github.com/kataras/iris"
-	"go.mongodb.org/mongo-driver/bson"
-	"io"
-	"os"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
 )
 
-const FilePath = "./img"
+func main() {
+	src.ConnectDB()
+	defer src.DisconnectDb()
+	app := iris.New()
+	app.Use(recover.New())
+	app.Use(logger.New())
 
-func Register(ctx iris.Context) {
-	// get info
-	Name := ctx.FormValue("name")
-	Email := ctx.FormValue("email")
-	Pass := ctx.FormValue("pass")
-	// get img
-	file, info, err := ctx.FormFile("img")
-	if err != nil {
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.Application().Logger().Warnf("Error while uploading: %v", err.Error())
-		return
-	}
-	defer file.Close()
-	filename := info.Filename
-	out, err := os.OpenFile(FilePath + filename, os.O_WRONLY | os.O_CREATE, 0666)
-	if err != nil {
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.Application().Logger().Warnf("Error while preparing the new file: %v", err.Error())
-		return
-	}
-	defer out.Close()
-	_, _ = io.Copy(out, file)
-	ImgPath := FilePath + filename
+	crs := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+	})
 
-	// create User info
-	UserData := User {
-		Username: Name,
-		Email: Email,
-		Password: Pass,
-		ImgPath: ImgPath,
-	}
-	var result User
-	collection := Client.Database("WithYou").Collection("UserInfo")
+	api := app.Party("/api", crs).AllowMethods(iris.MethodOptions)
 
-	// find email
-	filter := bson.M{"email": Email}
-	err = collection.FindOne(context.TODO(), filter).Decode(&result)
-	if err == nil {
-		RtData := RtMsg {
-			Msg: "Email has existed",
-			Code: -1,
-		}
-		_, _ = ctx.JSON(RtData)
-		return
-	}
-
+	api.Post("/login", src.Login)
 
 	api.Post("/register", src.Register)
 
@@ -75,9 +42,4 @@ func Register(ctx iris.Context) {
 
 	_ = app.Run(iris.Addr(":8080"))
 
-	RtData := RtMsg {
-		Msg: "Register succeed",
-		Code: 0,
-	}
-	_, _ = ctx.JSON(RtData)
 }
